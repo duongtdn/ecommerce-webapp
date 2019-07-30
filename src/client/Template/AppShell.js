7e9390565ffb982b98bd4ed782ef1635724cfad7
duongtdn <duongtdn@gmail.com>
2019-07-30 08:17:48 +0700

    start design browse view

--- src/client/Template/AppShell.js
+++ src/client/Template/AppShell.js
@@ -17,6 +17,55 @@ const routes = {
   error: Error
 }
 
+const courses = [
+  {
+    id: 'c-01',
+    title: 'Basic C Programming for Embedded System',
+    snippet: 'C Programming for beginner, good for one who want to learn about C and Embedded Programming',
+    description: 'c01.json',
+    price: 499,
+    promotion: [
+
+    ],
+    categories: ['Embedded Programming'],
+    tests: [
+      {
+        title: 'Mid-term Exam',
+        description: 'Mid-term Test for course Embedded - 01',
+        exam: 'c-01-m'
+      },
+      {
+        title: 'Final Exam',
+        description: 'Final Test for course Embedded - 01',
+        exam: 'c-01-f'
+      }
+    ]
+  },
+  {
+    id: 'c-02',
+    title: 'Advanced C Programming for Embedded System',
+    snippet: 'Advanced C Programming, ARM embedded processor and with pratice on famous STM32 series',
+    description: 'c02.json',
+    price: 799,
+    promotion: [
+      
+    ],
+    categories: ['Embedded Programming', 'ARM Programming'],
+    tests: [
+      {
+        title: 'Mid-term Exam',
+        description: 'Mid-term Test for course Embedded - 01',
+        exam: 'c-02-m'
+      },
+      {
+        title: 'Final Exam',
+        description: 'Final Test for course Embedded - 01',
+        exam: 'c-02-f'
+      }
+    ]
+  }
+]
+
 export default class AppShell extends Component {
   constructor(props) {
     super(props)
@@ -36,6 +85,7 @@ export default class AppShell extends Component {
                     activeRoute = {this.state.activeRoute}
                     navigate = {route => this.navigate(route)}
                     fallbackRoute = 'error'
+                    courses = {courses}
         />
       </div>
     )
--- src/client/Template/Page/Browse.js
+++ src/client/Template/Page/Browse.js
@@ -7,8 +7,44 @@ export default class Browse extends Component {
     super(props)
   }
   render() {
+    const categories = this._makeCategoriesListFromCourses()
     return (
-      <div> BROWSE PAGE </div>
+      <div className="w3-container">
+        {
+          categories.map( category => {
+            const courses = this.props.courses.filter( course => course.categories.indexOf(category) !== -1 )
+            return (
+              <div key = {category} className = "">
+                <div> <b>{category}</b> </div>
+                <div>
+                  {
+                    courses.map( course => {
+                      return (
+                        <div key = {course.id}>
+                          {course.title}
+                        </div>
+                      )
+                    })
+                  }
+                  <hr />
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     )
+  }
+  _makeCategoriesListFromCourses() {
+    const courses = this.props.courses
+    const categories = []
+    courses.forEach( course => {
+      course.categories.forEach( category => {
+        if (categories.indexOf(category) === -1) {
+          categories.push(category)
+        }
+      })
+    })
+    return categories
   }
 }
