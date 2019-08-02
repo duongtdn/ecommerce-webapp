a10496542b17313ee1acf6ea6097c0a9516cc6f0
duongtdn <duongtdn@gmail.com>
2019-08-02 23:07:39 +0700

    add scale prop

--- src/client/Template/Widget/Logo.js
+++ src/client/Template/Widget/Logo.js
@@ -7,9 +7,19 @@ export default class Logo extends Component {
     super(props)
   }
   render() {
+    const x = this.props.scale || 1
+    const text = {
+      fontSize: `${22*x}px`,
+      marginRight: `${2*x}px`
+    }
+    const icon = {
+      fontSize: `${18*x}px`,
+      left: `${2*x}px`,
+      top: `-${4*x}px`
+    }
     return (
       <span style={{fontFamily: "'Kaushan Script', cursive", position: 'relative'}}>
-        <span style={{color: "#2196F3", fontSize: "22px", marginRight: "2px"}}>Studi</span><span style={{color: "#607d8b", fontSize: "22px"}}>ha</span><i className="fas fa-seedling" style={{color: "#4CAF50", fontSize: "18px", position: "relative", left: "2px", top: "-4px"}}/>
+        <span style={{color: "#2196F3", fontSize: text.fontSize, marginRight: text.marginRight}}>Studi</span><span style={{color: "#607d8b", fontSize:text.fontSize}}>ha</span><i className="fas fa-seedling" style={{color: "#4CAF50", fontSize: icon.fontSize, position: "relative", left: icon.left, top: icon.top}}/>
       </span>
     )
   }
