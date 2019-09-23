3eea646e902ecd85014af7b882ced4ae702e0b5f
duongtdn <duongtdn@gmail.com>
2019-09-23 07:47:54 +0000

    fix calling sign-in, avoid passing env from click to sign-in

--- src/client/Template/Page/UnAuthen.js
+++ src/client/Template/Page/UnAuthen.js
@@ -7,7 +7,7 @@ export default function(props) {
     <div className="w3-container">
       <p className="w3-text-red w3-large bold">Unauthenticated</p>
       <p>Please Sign-in to see this page</p>
-      <p><button className="w3-button w3-blue" onClick={props.accountClient.signin}> Login | New Account </button></p>
+      <p><button className="w3-button w3-blue" onClick={e=>props.accountClient.signin()}> Login | New Account </button></p>
     </div>
   )
 }
