c79e68a269ca402ce2746af426e435b5f78b97aa
duongtdn <duongtdn@gmail.com>
2020-02-14 01:04:12 +0700

    add Sidebar

--- src/client/Template/Page/Browse.js
+++ src/client/Template/Page/Browse.js
@@ -177,6 +177,31 @@ class CoursePanel extends Component {
   }
 }
 
+class Sidebar extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const active = this.props.activeProgram
+    const programs = this.props.programs
+    return (
+      <div className="w3-sidebar w3-bar-block w3-border-right" style={{width: '250px', padding: '16px 0'}}>
+        {
+          programs.map(program => {
+            return (
+              <a  key={program.id}
+                  href={`/browse/${program.id}`}
+                  className={`w3-bar-item w3-button ${program.id===active?'w3-pale-blue bold':''}`} >
+                {program.title}
+              </a>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
 export default class Browse extends Component {
   constructor(props) {
     super(props)
@@ -188,29 +213,30 @@ export default class Browse extends Component {
     if (!program) { return (<div className="w3-container w3-text-red"> 404 Page not found </div>) }
     const courses = this.props.courses.filter( course => course.programs.indexOf(program.id) !== -1 )
     return (
-      <div className="w3-container">
-        <div>
-          <span className="w3-hide-small">
-            <a href="/browse" className="w3-tag w3-blue w3-round" style={{textDecoration: 'none'}}>Program</a>
-            {' '}
-            <i className="fa fa-arrow-right" />
-          </span>
-          <span className="w3-text-blue"> {program.title} </span>
-          <hr />
+      <div>
+        <div className="w3-hide-small">
+          <Sidebar programs={programs} activeProgram={prog} />
         </div>
-        <ul className="w3-ul">
-          {
-            courses.map( course => {
-              const promos = this.props.promos? this.props.promos.filter( promo => promo.target.indexOf(course.id) !== -1 ) : []
-              return (
-                <li key = {course.id} style={{padding: '0 0 8px 0'}}>
-                  <LevelBar course = {course} />
-                  <CoursePanel course = {course} promos = {promos} user={this.props.user} me={this.props.me} />
-                </li>
-              )
-            })
-          }
-        </ul>
+        <div className="w3-container w3-cell-row">
+          <div className="w3-cell w3-hide-small" style={{width: '250px'}} />
+          <div className="w3-cell">
+            {/* <span className="w3-text-blue"> {program.title} </span>
+            <hr /> */}
+            <ul className="w3-ul">
+              {
+                courses.map( course => {
+                  const promos = this.props.promos? this.props.promos.filter( promo => promo.target.indexOf(course.id) !== -1 ) : []
+                  return (
+                    <li key = {course.id} style={{padding: '0 0 8px 0'}}>
+                      <LevelBar course = {course} />
+                      <CoursePanel course = {course} promos = {promos} user={this.props.user} me={this.props.me} />
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
