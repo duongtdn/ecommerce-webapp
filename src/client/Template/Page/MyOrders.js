fca836fe64b22b049e7cab29f3c7c3c915e7a5f7
duongtdn <duongtdn@gmail.com>
2020-02-20 04:09:27 +0000

    localize MyOrders

--- src/client/Template/Page/MyOrders.js
+++ src/client/Template/Page/MyOrders.js
@@ -1,6 +1,7 @@
 "use strict"
 
 import React, { Component } from 'react'
+import { FormattedMessage, injectIntl } from 'react-intl'
 
 import xhttp from '@realmjs/xhttp-request'
 
@@ -9,12 +10,12 @@ import { getDay, localeString } from '../../lib/util'
 import UnAuthen from './UnAuthen'
 
 const _dict = {
-  'cod': 'Pay when received items',
-  'bank': 'Bank transfer',
-  'card': 'Paid using Internation card'
+  'cod': "myorders.dict.cod",
+  'bank': "myorders.dict.bank",
+  'card': "myorders.dict.card",
 }
 
-class OrderCard extends Component {
+const OrderCard = injectIntl(class extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -24,10 +25,11 @@ class OrderCard extends Component {
     }
   }
   render() {
+    const intl = this.props.intl
     const order = this.props.order
     const subTotal = order.items.reduce( (acc, cur) => acc + (cur.checked ? cur.price : 0), 0 )
     return (
-      <div className="w3-card w3-padding" style={{margin: '16px auto', maxWidth: '920px'}}>
+      <div className="w3-card w3-padding" style={{margin: '16px auto',}}>
         <div className={order.status ==='deleted'?'w3-text-grey italic':'w3-text-blue'}>
           <div className="w3-cell-row">
             <div className="w3-cell">
@@ -55,39 +57,39 @@ class OrderCard extends Component {
           {
             this.state.showDetail?
               <span className="w3-small italic cursor-pointer w3-text-blue" style={{textDecoration: 'none'}} onClick={e => this.hideOrderDetail()} >
-                hide detail
+                <FormattedMessage id="label.hide_detail" />
               </span>
             :
               <span className="w3-small italic cursor-pointer w3-text-blue" style={{textDecoration: 'none'}} onClick={e => this.showOrderDetail()} >
-                show detail...
+                <FormattedMessage id="label.show_detail" />...
               </span>
           }
         </div>
         {/* Detail order */}
         <div className={`w3-border-top w3-border-grey ${this.state.showDetail?'':'w3-hide'}`} style={{margin: '16px 0'}}>
 
-          <p className="w3-small "> Status: {order.status.toUpperCase()} </p>
+          <p className="w3-small "> <FormattedMessage id="order.receipt.status" />: {order.status.toUpperCase()} </p>
 
           <p className="w3-small ">
-            Payment: {' '}
+            <FormattedMessage id="label.payment" />: {' '}
             {
-              _dict[order.paymentMethod]
+              intl.formatMessage({id: _dict[order.paymentMethod]})
             }
           </p>
 
           <p className="w3-small ">
-            <span className="w3-text-grey"> Delivery to </span> <br />
+            <span className="w3-text-grey"> <FormattedMessage id="order.delivery_to" /> </span> <br />
             <span className="w3-padding bold w3-text-blue-grey"> {order.delivery.fullName} </span> <br/>
             <span className="w3-padding w3-text-blue-grey"> {order.delivery.phone} </span> <br/>
             <span className="w3-padding w3-text-blue-grey"> {order.delivery.address} </span>
           </p>
 
-          <p className="w3-text-grey w3-small"> Item Lists </p>
+          <p className="w3-text-grey w3-small"> <FormattedMessage id="order.receipt.item_lists" /> </p>
           <table className="w3-table w3-border w3-bordered w3-small">
             <thead>
               <tr className="w3-blue">
-                <th className = "w3-border-right">Item</th>
-                <th style={{textAlign: 'right'}} >Value ({'\u20ab'})</th>
+                <th className = "w3-border-right"><FormattedMessage id="order.label.Item" /></th>
+                <th style={{textAlign: 'right'}} ><FormattedMessage id="order.label.Value" /> ({'\u20ab'})</th>
               </tr>
             </thead>
             <tbody>
@@ -115,7 +117,7 @@ class OrderCard extends Component {
             </tbody>
             <tfoot>
               <tr className="w3-pale-blue">
-                <th className = "w3-border-right">Total</th>
+                <th className = "w3-border-right"><FormattedMessage id="order.label.total" /></th>
                 <th className="w3-text-orange" style={{textAlign: 'right'}} > {localeString(subTotal)} </th>
               </tr>
             </tfoot>
@@ -127,14 +129,18 @@ class OrderCard extends Component {
                 {
                   !this.state.showCancelReasonInput?
                     <div style={{textAlign: 'right'}}>
-                      <button className="w3-button w3-small w3-text-red" onClick={e=>this.showCancelReason()}> <span className="bold">&times;</span> Cancel Order </button>
+                      <button className="w3-button w3-small w3-text-red" onClick={e=>this.showCancelReason()}>
+                        <span className="bold">&times;</span> <FormattedMessage id="button.cancel_order" />
+                      </button>
                     </div>
                   :
                     <div>
-                      <p className="italic w3-text-grey w3-small"> Please share us why you would like to cancel this order</p>
+                      <p className="italic w3-text-grey w3-small"> <FormattedMessage id="label.cancel_order_message" /> </p>
                       <textarea className="w3-small w3-text-grey" style={{width: '100%', padding: '3px'}} value={this.state.cancelReason} onChange={e => this.handleReasonInputChange(e)} />
                       <div style={{margin: '8px 0', textAlign: 'right'}}>
-                        <button className="w3-button w3-small w3-red" onClick={e=>this.cancelOrder(order)}> Cancel Order </button>
+                        <button className="w3-button w3-small w3-red" onClick={e=>this.cancelOrder(order)}>
+                          <FormattedMessage id="button.cancel_order" />
+                        </button>
                       </div>
                     </div>
                 }
@@ -183,7 +189,7 @@ class OrderCard extends Component {
       this.props.showPopup('info', { closeBtn: true, message: `Error: Cannot connect to server!`, align: 'left' })
     })
   }
-}
+})
 
 export default class MyOrders extends Component {
   constructor(props) {
@@ -194,7 +200,9 @@ export default class MyOrders extends Component {
     const orders = this.props.me.orders
     return (
       <div className="w3-container">
-        <div style={{margin: '16px auto', maxWidth: '920px'}}> <h4 className="w3-text-blue"> Orders </h4> </div>
+        <div style={{maxWidth: '920px'}}>
+          <h3 className="w3-text-blue"> <FormattedMessage id="myorders.label.title" /> </h3>
+        </div>
         {
           orders.sort( (a, b) => b.createdAt - a.createdAt ).map(order => (
             <OrderCard key = {order.number} order = {order} {...this.props} />
