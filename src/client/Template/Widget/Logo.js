f8f7b292fc6bed5c394fd5cea0543ed53df1c243
duongtdn <duongtdn@gmail.com>
2020-02-25 19:42:55 +0700

    add option to use html logo

--- src/client/Template/Widget/Logo.js
+++ src/client/Template/Widget/Logo.js
@@ -7,9 +7,18 @@ export default class Logo extends Component {
     super(props)
   }
   render() {
+    if (this.props.source === 'png') {
+      return ( <img height="60px" src="/public/logo.png" /> )
+    }
+    if (this.props.source === 'png-white') {
+      return ( <img height="51px" src="/public/logo_bg.png" /> )
+    }
     return (
-      // <img height="60px" src="/public/logo.png" />
-      <img height="51px" src="/public/logo_bg.png" />
+      <span style={{fontFamily: 'Kanit',fontSize: '36px', fontWeight: 'bold', position: 'relative'}}>
+        <span style={{color: '#0050EF', marginRight: '4px'}}>Studi</span>
+        <span style={{color: '#3A3A3A', marginRight: '6px'}}>ha</span>
+        <img src="/public/icon.png" width="24px" />
+      </span>
     )
   }
 }
