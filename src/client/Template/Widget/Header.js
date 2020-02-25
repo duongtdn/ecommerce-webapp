2de969818f3ed1585e079d583dda99087289c1e3
duongtdn <duongtdn@gmail.com>
2020-02-25 17:53:24 +0700

    adjust right position of dropdown responsive

--- src/client/Template/Widget/Header.js
+++ src/client/Template/Widget/Header.js
@@ -9,13 +9,29 @@ import storage from '../../lib/storage'
 class UserSnipet extends Component {
   constructor(props) {
     super(props)
+    this.state = { righEdge: 0 }
     this.signout = this.signout.bind(this)
+    this.ref = React.createRef()
+    this.adjustDropdownRightPosition = this.adjustDropdownRightPosition.bind(this)
+  }
+  componentDidMount() {
+    this.adjustDropdownRightPosition()
+    window.addEventListener('resize', this.adjustDropdownRightPosition, false)
+  }
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.adjustDropdownRightPosition)
+  }
+  adjustDropdownRightPosition() {
+    const node = this.ref.current
+    const rect = node.getBoundingClientRect()
+    const righEdge = innerWidth - (rect.left + rect.width)
+    this.setState({ righEdge: `${righEdge}px` })
   }
   render() {
     const user = this.props.user
     const avata = this._getUserAvata()
     return (
-      <div className="w3-bar-item w3-dropdown-hover" style={{margin: '4px 0', padding: '3px 16px', cursor: 'pointer'}}>
+      <div ref={this.ref} className="w3-bar-item w3-dropdown-hover" style={{margin: '4px 0', padding: '3px 16px', cursor: 'pointer'}}>
         <div className="w3-white">
           <img className="w3-circle w3-border w3-border-white"style={{width: '35px'}} src={avata} />
           {' '}
@@ -23,7 +39,7 @@ class UserSnipet extends Component {
           {' '}
           <i className="w3-text-dark-grey fa fa-ellipsis-v" />
         </div>
-        <div className='w3-dropdown-content w3-bar-block' style={{padding: '4px', right: '4px'}}>
+        <div className='w3-dropdown-content w3-bar-block' style={{padding: '4px', right: this.state.righEdge}}>
           <button className="w3-bar-item w3-button w3-border w3-green" style={{textAlign: 'center'}} onClick={this.signout}>
             <i className="fas fa-sign-out-alt" style={{marginRight: '4px'}} /> <FormattedMessage id="button.signout" />
           </button>
@@ -96,7 +112,7 @@ class ShoppingCart extends Component {
   }
   render() {
     return (
-      <span className="w3-large w3-bar-item w3-button w3-hover-none" style={{position: 'relative', margin: '0 8px'}} onClick={e => this.props.navigate('order')}>
+      <span className="w3-large w3-bar-item w3-button w3-hover-none" style={{position: 'relative', margin: '5px 8px'}} onClick={e => this.props.navigate('order')}>
         <i className={`fas fa-shopping-cart ${this.state.itemCount > 0 ? 'w3-text-blue' : 'w3-text-light-blue'}`} />
         {
           this.state.itemCount > 0 ?
@@ -128,7 +144,7 @@ export default class Header extends Component {
 
             <div className="w3-bar-item w3-right" style={{padding: '8px'}}>
               <div className="" style={{display: 'inline-block', verticalAlign: 'bottom'}}>
-                <button className="w3-bar-item w3-button w3-green" style={{marginRight: '68px'}} onClick={e => this.props.showPopup('activation')} >
+                <button className="w3-bar-item w3-button w3-green" style={{marginTop: '3px', marginRight: '68px'}} onClick={e => this.props.showPopup('activation')} >
                   <FormattedMessage id="button.activate_course" />
                 </button>
                 <div className="w3-bar-item w3-border-left" style={{height: '40px', borderLeftWidth: '2px !important'}} />
