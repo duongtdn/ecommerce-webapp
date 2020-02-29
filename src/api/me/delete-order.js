c54d73c32df872c1fb1b653488766911260768e4
duongtdn <duongtdn@gmail.com>
2020-02-29 18:23:36 +0700

    change api behavior: delete order will remove its and active code from database

--- src/api/me/delete-order.js
+++ src/api/me/delete-order.js
@@ -2,18 +2,50 @@
 
 const {authen} = require('../lib/authen')
 
+function validateParams() {
+  return function(req, res, next) {
+    if (req.body.number) {
+      next()
+    } else {
+      res.status(400).json({ error: 'Bad parameters'})
+    }
+  }
+}
+
+function getActivateCode(helpers) {
+  return function(req, res, next) {
+    helpers.Database.ORDER.find({uid: `= ${req.uid}`, number: `= ${req.body.number}`})
+    .then( data => {
+      if (data && data.length > 0) {
+        req.code = data[0].activationCode
+        next()
+      } else {
+        res.status(404).json({ error: 'Not found'})
+      }
+    })
+    .catch(err => {
+      helpers.alert && helpers.alert({
+        message: 'Database operation failed',
+        action: 'FIND order',
+        error: err
+      })
+      res.status(500).json({ reason: 'Failed to Access Database' })
+    })
+  }
+}
+
 function deleteOrder(helpers) {
   return function(req, res, next) {
     const uid = req.uid
     const number = req.body.number
-    if (!number) {
-      res.status(400).json({ error: 'Bad parameters'})
-      return
-    }
-    const reason = req.body.reason
-    const notes = {}
-    notes[(new Date()).getTime()] = `User has deleted the order. Reason: ${reason}`
-    helpers.Database.ORDER.update({uid, number}, { status: 'deleted', notes })
+    helpers.Database.batchWrite({
+      ORDER: {
+        remove: [{uid, number}]
+      },
+      ACTIVECODE: {
+        remove: [{ code: req.code }]
+      }
+    })
     .then( () => res.status(200).json({ status: 'success' }))
     .catch(err => {
       helpers.alert && helpers.alert({
@@ -26,4 +58,4 @@ function deleteOrder(helpers) {
   }
 }
 
-module.exports = [authen, deleteOrder]
+module.exports = [authen, validateParams, getActivateCode, deleteOrder]
